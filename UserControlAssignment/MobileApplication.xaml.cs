1e8c1fbef1d44a3800678b4a7faf7860cf3ad334
www.abcom.in <www.abcom.in@DESKTOP-HA16J39>
2022-10-05 21:24:23 +0530

    Add project files.

---
+++ UserControlAssignment/MobileApplication.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControlAssignment
+{
+    /// <summary>
+    /// Interaction logic for MobileApplication.xaml
+    /// </summary>
+    public partial class MobileApplication : UserControl
+    {
+        public MobileApplication()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
